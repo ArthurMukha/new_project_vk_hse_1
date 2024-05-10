ba99b4b76b223b29e196f818709fdc251cd0c6cf
Jaba <154435190+ArthurMukha@users.noreply.github.com>
2024-05-11 00:55:47 +0300

    Update init_models.py

--- Models/init_models.py
+++ Models/init_models.py
@@ -68,7 +68,7 @@ def nima_pred(image):
     return np.sum(scores * np.arange(1, 11, 1))
 
 def mobilenet_pred(image_path):
-    img = Image.open(image_page).resize((224,224)).convert("RGB")
+    img = Image.open(image_path).resize((224,224)).convert("RGB")
     img = np.array(img)
     x = np.expand_dims(img, axis=0)
     x = MN_model.predict(img, batch_size=1)
